d31ee6c01a3f06b4d7a7f71cdb5151f9fa7bda23
Jiaxi Wu <jiaxwu159@gmail.com>
2019-05-24 18:08:04 -0400

    Added searchBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import CardList from './CardList'
-import {robots} from './robots';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots}  from './robots';
 
-const App = () => {
-    return(
-        <CardList robots={robots}/>
-    );
-};
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    };
+
+    onSearchChange(event){
+        console.log(event)
+    };
+
+        render() {
+            return(
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={this.state.robots}/>
+                </div>
+            );
+        }
+    };
 
 export default App;
