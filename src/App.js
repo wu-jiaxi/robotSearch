b815f6b518b5d68aeda3e6ecb73ba29e69465707
Jiaxi Wu <jiaxwu159@gmail.com>
2019-05-27 19:04:49 -0400

    Search Bar
    Got search bar to search

--- src/App.js
+++ src/App.js
@@ -14,16 +14,20 @@ class App extends Component {
         }
     };
 
-    onSearchChange(event){
-        console.log(event);
+    onSearchChange = (event) => {
+        this.setState ({ searchfield: event.target.value })
+    
     };
 
         render() {
+            const filteredRobots = this.state.robots.filter( robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            });
             return(
                 <div className='tc'>
                     <h1>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={this.state.robots}/>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
                 </div>
             );
         }
